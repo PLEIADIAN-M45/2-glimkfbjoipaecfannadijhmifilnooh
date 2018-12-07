ed663032d554254b74fe56fbc0b3f8e15f6ecf3a
PLEIADIAN.M45 <34002867+PLEIADIAN-M45@users.noreply.github.com>
2018-12-08 00:01:20 +0800

    2122

--- 2018.09.21.2/module/factory.js
+++ 2018.09.21.2/module/factory.js
@@ -20,29 +20,29 @@ define(['angular', 'Dexie', 'moment', 'material', 'semantic'], function(angular,
         this.port = location.port;
         this.path = location.pathname.split('?')[0].split('.')[0].split('/').pop().toLowerCase();
         this.host = (location.port) ? { "8876": "wa111", "26": "wa111", "35": "wa111", "17": "wa111", "16": "ku711" } [location.port] : location.host.split(".")[1];
-        this.route = {
-            "wa111": {
-                "login": "login",
-                "index": "home",
-                "memberlist": "list",
-                "membermodify": "edit",
-                "depositbonus": "bonus",
-                "igetmemberinfo": "logs",
-                "samebrowserlist": "logs",
-                "deltabank": "cash",
-                "deltaonline": "cash",
-                "deltawechat": "cash",
-                "deltaalipay": "cash",
-                "withdrawalsbank": "cash",
-                "astropaywithdrawals": "cash"
+        this.route                     = {
+            "wa111"                    : {
+                "login"                : "login",
+                "index"                : "home",
+                "memberlist"           : "list",
+                "membermodify"         : "edit",
+                "depositbonus"         : "bonus",
+                "igetmemberinfo"       : "logs",
+                "samebrowserlist"      : "logs",
+                "deltabank"            : "cash",
+                "deltaonline"          : "cash",
+                "deltawechat"          : "cash",
+                "deltaalipay"          : "cash",
+                "withdrawalsbank"      : "cash",
+                "astropaywithdrawals"  : "cash"
             },
-            "ku711": {
-                "signin": "login",
-                "member": "home",
-                "memberinfomanage": "list",
-                "editmemberinfomanage": "edit",
-                "bonuslog": "bonus",
-                "memberloginlog": "log"
+            "ku711"                    : {
+                "signin"               : "login",
+                "member"               : "home",
+                "memberinfomanage"     : "list",
+                "editmemberinfomanage" : "edit",
+                "bonuslog"             : "bonus",
+                "memberloginlog"       : "log"
             }
         } [this.host][this.path];
         this.operator = localStorage.operator;
