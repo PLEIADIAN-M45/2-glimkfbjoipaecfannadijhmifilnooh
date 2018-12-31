8598128698ac758c17f61ed313014cf6023e78d7
PLEIADIAN.M45 <34002867+PLEIADIAN-M45@users.noreply.github.com>
2018-12-31 16:34:53 +0800

    12

---
+++ 2018.09.21.2/app/window.service.js
@@ -0,0 +1,183 @@
+
+Status   Name               DisplayName                           
+------   ----               -----------                           
+Stopped  {0CBD4F48-3751-... {0CBD4F48-3751-475D-BE88-4F271385B672}
+Running  AdobeARMservice    Adobe Acrobat Update Service          
+Running  AeLookupSvc        Application Experience                
+Stopped  ALG                Application Layer Gateway Service     
+Stopped  AppIDSvc           Application Identity                  
+Stopped  Appinfo            Application Information               
+Stopped  AppMgmt            Application Management                
+Stopped  aspnet_state       ASP.NET ????                          
+Running  AudioEndpointBu... Windows Audio Endpoint Builder        
+Running  AudioSrv           Windows Audio                         
+Stopped  AxInstSV           ActiveX Installer (AxInstSV)          
+Stopped  BDESVC             BitLocker Drive Encryption Service    
+Running  BFE                Base Filtering Engine                 
+Running  BITS               Background Intelligent Transfer Ser...
+Running  Browser            Computer Browser                      
+Stopped  bthserv            Bluetooth Support Service             
+Stopped  CertPropSvc        Certificate Propagation               
+Stopped  clr_optimizatio... Microsoft .NET Framework NGEN v2.0....
+Stopped  clr_optimizatio... Microsoft .NET Framework NGEN v2.0....
+Stopped  clr_optimizatio... Microsoft .NET Framework NGEN v4.0....
+Stopped  clr_optimizatio... Microsoft .NET Framework NGEN v4.0....
+Stopped  COMSysApp          COM+ System Application               
+Stopped  cphs               Intel(R) Content Protection HECI Se...
+Running  CryptSvc           Cryptographic Services                
+Running  CscService         Offline Files                         
+Running  DcomLaunch         DCOM Server Process Launcher          
+Stopped  defragsvc          Disk Defragmenter                     
+Running  Dhcp               DHCP Client                           
+Running  DiagTrack          Diagnostics Tracking Service          
+Running  Dnscache           DNS Client                            
+Stopped  dot3svc            Wired AutoConfig                      
+Running  DPS                Diagnostic Policy Service             
+Running  Dscrp              Security Driver Extension             
+Running  EapHost            Extensible Authentication Protocol    
+Stopped  EFS                Encrypting File System (EFS)          
+Stopped  ehRecvr            Windows Media Center Receiver Service 
+Stopped  ehSched            Windows Media Center Scheduler Service
+Running  EMET_Service       Microsoft EMET Service                
+Running  eventlog           Windows Event Log                     
+Running  EventSystem        COM+ Event System                     
+Running  EvtEng             Intel(R) PROSet/Wireless Event Log    
+Stopped  Fax                ??                                    
+Stopped  fdPHost            Function Discovery Provider Host      
+Stopped  FDResPub           Function Discovery Resource Publica...
+Running  FontCache          Windows Font Cache Service            
+Running  FontCache3.0.0.0   Windows Presentation Foundation Fon...
+Stopped  GoogleChromeEle... Google Chrome Elevation Service       
+Running  gpsvc              Group Policy Client                   
+Stopped  gupdate            Google ???? (gupdate)                 
+Stopped  gupdatem           Google ???? (gupdatem)                
+Running  hidserv            Human Interface Device Access         
+Stopped  hkmsvc             Health Key and Certificate Management 
+Stopped  HomeGroupListener  HomeGroup Listener                    
+Stopped  HomeGroupProvider  HomeGroup Provider                    
+Running  IAStorDataMgrSvc   Intel(R) Rapid Storage Technology     
+Stopped  idsvc              Windows CardSpace                     
+Stopped  IEEtwCollectorS... Internet Explorer ETW Collector Ser...
+Running  igfxCUIService2... Intel(R) HD Graphics Control Panel ...
+Running  IKEEXT             IKE and AuthIP IPsec Keying Modules   
+Stopped  Intel(R) Capabi... Intel(R) Capability Licensing Servi...
+Stopped  IPBusEnum          PnP-X IP Bus Enumerator               
+Running  iphlpsvc           IP Helper                             
+Running  jhi_service        Intel(R) Dynamic Application Loader...
+Running  KeyIso             CNG Key Isolation                     
+Running  Killer Service V2  Killer Service V2                     
+Stopped  KtmRm              KtmRm for Distributed Transaction C...
+Running  LanmanServer       Server                                
+Running  LanmanWorkstation  Workstation                           
+Stopped  lltdsvc            Link-Layer Topology Discovery Mapper  
+Running  lmhosts            TCP/IP NetBIOS Helper                 
+Running  LMS                Intel(R) Management and Security Ap...
+Stopped  Mcx2Svc            Media Center Extender Service         
+Running  MMCSS              Multimedia Class Scheduler            
+Running  MpsSvc             Windows Firewall                      
+Stopped  MSDTC              Distributed Transaction Coordinator   
+Stopped  MSiSCSI            Microsoft iSCSI Initiator Service     
+Stopped  msiserver          Windows Installer                     
+Stopped  MxService          MxService                             
+Stopped  napagent           Network Access Protection Agent       
+Stopped  Netlogon           Netlogon                              
+Running  Netman             Network Connections                   
+Stopped  NetMsmqActivator   Net.Msmq Listener Adapter             
+Stopped  NetPipeActivator   Net.Pipe Listener Adapter             
+Running  netprofm           Network List Service                  
+Stopped  NetTcpActivator    Net.Tcp Listener Adapter              
+Stopped  NetTcpPortSharing  Net.Tcp Port Sharing Service          
+Running  NlaSvc             Network Location Awareness            
+Running  nsi                Network Store Interface Service       
+Running  NVDisplay.Conta... NVIDIA Display Container LS           
+Running  NvTelemetryCont... NVIDIA Telemetry Container            
+Stopped  ose64              Office 64 Source Engine               
+Running  osppsvc            Office Software Protection Platform   
+Stopped  p2pimsvc           Peer Networking Identity Manager      
+Stopped  p2psvc             Peer Networking Grouping              
+Running  PcaSvc             Program Compatibility Assistant Ser...
+Running  Pcrps              Network Driver Extension              
+Stopped  PeerDistSvc        BranchCache                           
+Stopped  PerfHost           Performance Counter DLL Host          
+Stopped  pla                Performance Logs & Alerts             
+Running  PlugPlay           Plug and Play                         
+Stopped  PNRPAutoReg        PNRP Machine Name Publication Service 
+Stopped  PNRPsvc            Peer Name Resolution Protocol         
+Running  PolicyAgent        IPsec Policy Agent                    
+Running  Power              Power                                 
+Running  ProfSvc            User Profile Service                  
+Stopped  ProtectedStorage   Protected Storage                     
+Running  QPCore             QPCore Service                        
+Stopped  QWAVE              Quality Windows Audio Video Experience
+Stopped  RasAuto            Remote Access Auto Connection Manager 
+Stopped  RasMan             Remote Access Connection Manager      
+Running  RegSrvc            Intel(R) PROSet/Wireless Registry S...
+Stopped  RemoteAccess       Routing and Remote Access             
+Stopped  RemoteRegistry     Remote Registry                       
+Running  RpcEptMapper       RPC Endpoint Mapper                   
+Stopped  RpcLocator         Remote Procedure Call (RPC) Locator   
+Running  RpcSs              Remote Procedure Call (RPC)           
+Running  SamSs              Security Accounts Manager             
+Stopped  SCardSvr           Smart Card                            
+Running  Schedule           Task Scheduler                        
+Stopped  SCPolicySvc        Smart Card Removal Policy             
+Stopped  SDRSVC             Windows Backup                        
+Stopped  seclogon           Secondary Logon                       
+Running  SENS               System Event Notification Service     
+Stopped  SensrSvc           Adaptive Brightness                   
+Stopped  SessionEnv         Remote Desktop Configuration          
+Stopped  SharedAccess       Internet Connection Sharing (ICS)     
+Running  ShellHWDetection   Shell Hardware Detection              
+Stopped  SNMPTRAP           SNMP Trap                             
+Running  Spooler            Print Spooler                         
+Stopped  sppsvc             Software Protection                   
+Stopped  sppuinotify        SPP Notification Service              
+Running  SSDPSRV            SSDP Discovery                        
+Stopped  SstpSvc            Secure Socket Tunneling Protocol Se...
+Stopped  stisvc             Windows Image Acquisition (WIA)       
+Stopped  swprv              Microsoft Software Shadow Copy Prov...
+Running  SysMain            Superfetch                            
+Stopped  TabletInputService Tablet PC Input Service               
+Stopped  TapiSrv            Telephony                             
+Running  TeamViewer         TeamViewer 12                         
+Stopped  TermService        Remote Desktop Services               
+Running  Themes             Themes                                
+Stopped  THREADORDER        Thread Ordering Server                
+Running  TrkWks             Distributed Link Tracking Client      
+Stopped  TrustedInstaller   Windows Modules Installer             
+Stopped  UI0Detect          Interactive Services Detection        
+Stopped  UmRdpService       Remote Desktop Services UserMode Po...
+Stopped  upnphost           UPnP Device Host                      
+Running  UxSms              Desktop Window Manager Session Manager
+Running  VaultSvc           Credential Manager                    
+Stopped  vds                Virtual Disk                          
+Stopped  VSS                Volume Shadow Copy                    
+Stopped  W32Time            Windows Time                          
+Stopped  WatAdminSvc        Windows ??????                        
+Stopped  wbengine           Block Level Backup Engine Service     
+Stopped  WbioSrvc           Windows Biometric Service             
+Stopped  wcncsvc            Windows Connect Now - Config Registrar
+Stopped  WcsPlugInService   Windows Color System                  
+Running  WdiServiceHost     Diagnostic Service Host               
+Running  WdiSystemHost      Diagnostic System Host                
+Stopped  WebClient          WebClient                             
+Stopped  Wecsvc             Windows Event Collector               
+Stopped  wercplsupport      Problem Reports and Solutions Contr...
+Stopped  WerSvc             Windows Error Reporting Service       
+Running  WinDefend          Windows Defender                      
+Running  WinHttpAutoProx... WinHTTP Web Proxy Auto-Discovery Se...
+Running  Winmgmt            Windows Management Instrumentation    
+Stopped  WinRM              Windows Remote Management (WS-Manag...
+Running  Wlansvc            WLAN AutoConfig                       
+Stopped  wmiApSrv           WMI Performance Adapter               
+Stopped  WMPNetworkSvc      Windows Media Player Network Sharin...
+Stopped  WPCSvc             Parental Controls                     
+Stopped  WPDBusEnum         Portable Device Enumerator Service    
+Running  wscsvc             Security Center                       
+Running  WSearch            Windows Search                        
+Running  wuauserv           Windows Update                        
+Running  wudfsvc            Windows Driver Foundation - User-mo...
+Stopped  WwanSvc            WWAN AutoConfig                       
+Running  ZeroConfigService  Intel(R) PROSet/Wireless Zero Confi...
+
+
